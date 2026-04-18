a5cf1528e60d9079f4635951bed4fa26c367f946
MOMTH666 <safetytravelservice21@gmail.com>
2026-04-18 18:41:06 +0000

    Create JokeGenerator.js component to fetch and display jokes

---
+++ src/components/JokeGenerator.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const JokeGenerator = () => {
+    const [joke, setJoke] = useState('');
+
+    const fetchJoke = async () => {
+        try {
+            const response = await fetch('https://v2.jokeapi.dev/joke/Any');
+            const data = await response.json();
+            if (data.setup) {
+                setJoke(`${data.setup} - ${data.delivery}`);
+            } else {
+                setJoke(data.joke);
+            }
+        } catch (error) {
+            setJoke('Failed to fetch a joke.');
+        }
+    };
+
+    useEffect(() => {
+        fetchJoke();
+    }, []);
+
+    return (
+        <div>
+            <p>{joke}</p>
+            <button onClick={fetchJoke}>Generate New Joke</button>
+        </div>
+    );
+};
+
+export default JokeGenerator;
